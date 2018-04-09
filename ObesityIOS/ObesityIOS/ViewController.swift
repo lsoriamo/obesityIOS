892643a606641ca94896a0c257d05e28ec33d3e9
Jorge Amores <jamoresortiz@gmail.com>
2018-04-09 10:36:17 +0200

    Creación de proyecto e inclusión de diversos pods necesarios para conexión con Firebase

---
+++ ObesityIOS/ObesityIOS/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ObesityIOS
+//
+//  Created by Jorge Amores Ortiz on 9/4/18.
+//  Copyright © 2018 Preobar. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        FirebaseApp.configure();
+        var ref = Database.database().reference();
+        ref.child("users/2IaSdrayoQStcjVXT2g01i0ebrl2/info")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
