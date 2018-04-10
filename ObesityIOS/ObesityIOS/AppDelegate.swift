523cdca7eb6fbeb4ac76e8b7f839aa4ee9c514d2
Jorge Amores <jamoresortiz@gmail.com>
2018-04-10 17:09:32 +0200

    Se realiza parte de login de usuario

--- ObesityIOS/ObesityIOS/AppDelegate.swift
+++ ObesityIOS/ObesityIOS/AppDelegate.swift
@@ -9,9 +9,37 @@
 import UIKit
 import CoreData
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        // ...
+        if let error = error {
+            print("Error de google en el delegado")
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        // ...
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        // Perform any operations when the user disconnects from app here.
+        // ...
+    }
+    
+    @available(iOS 11.0, *)
+    func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+        -> Bool {
+            return GIDSignIn.sharedInstance().handle(url,
+                                                     sourceApplication:options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String,
+                                                     annotation: [:])
+    }
+}
+    
 
     var window: UIWindow?
 
@@ -21,6 +49,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Arranca la configuración de Firebase
         FirebaseApp.configure()
+        
+        // Función sin conexión de database
+        Database.database().isPersistenceEnabled = true
+        
+        // Logueo con cuenta de Google
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
 
--- ObesityIOS/ObesityIOS/ViewController.swift
+++ ObesityIOS/ObesityIOS/ViewController.swift
@@ -9,21 +9,101 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate{
 
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPass: UITextField!
+    
+    var email: String = ""
+    var pass: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        FirebaseApp.configure();
-        var ref = Database.database().reference();
-        ref.child("users/2IaSdrayoQStcjVXT2g01i0ebrl2/info")
+        //var ref = Database.database().reference();
+        //ref.child("users/2IaSdrayoQStcjVXT2g01i0ebrl2/info")
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        // ...
+        if let error = error {
+            print("Error de Google count en view")
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+                                                       accessToken: authentication.accessToken)
+        // ...
+    }
 
-
+    @IBAction func btnAccessUserAccount(_ sender: Any) {
+        email = tfEmail.text!
+        pass = tfPass.text!
+        
+        // Comprobación de que el email o contraseña no estén vacíos
+        
+        //TODO falta completar la comprobación de de patrón de email
+        if email == "" || pass == "" {
+            // Creando un elemento de Alert (Dialog en Android)
+            let alert = UIAlertController(title: "Error", message: "Introduce tu email y contraseña", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            // <-- Fin de Alert -->
+        } else {
+            
+            let user:User = User()
+            
+            user.email = email
+            user.passhash = pass
+            
+            Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                
+                if let error = error {
+                    print("ERROR")
+                    return
+                }
+                // Creando un elemento de Alert (Dialog en Android)
+                let alert = UIAlertController(title: "BIEN", message: "Login correcto", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Cerrar", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                // <-- Fin de Alert -->
+            }
+        }
+        
+    }
+    
+    // TODO Por acabar
+    @IBAction func btnAccessGoogleAccount(_ sender: Any) {
+        let user:User = User()
+        
+        user.email = email
+        user.passhash = pass
+        
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if let error = error {
+                // ...
+                return
+            }
+            // User is signed in
+            // ...
+        }
+    }
+    }
+    
 }
 
