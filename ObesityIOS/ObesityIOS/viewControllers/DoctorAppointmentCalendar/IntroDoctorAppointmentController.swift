3a1799aa08b4fe511174763d1e683aba6f6c3996
Jorge Amores <jamoresortiz@gmail.com>
2018-05-09 14:25:26 +0200

    Cambios en interfaz de citas

--- ObesityIOS/ObesityIOS/viewControllers/DoctorAppointmentCalendar/IntroDoctorAppointmentController.swift
+++ ObesityIOS/ObesityIOS/viewControllers/DoctorAppointmentCalendar/IntroDoctorAppointmentController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class IntroDoctorAppointmentController: UIViewController {
 
+    @IBOutlet weak var checkBoxNoMostrarInfo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        checkBoxNoMostrarInfo.setImage(UIImage(named: "ic_checkbox_default"), for: .normal)
+
+        checkBoxNoMostrarInfo.setImage(UIImage(named: "ic_checkbox_selected"), for: .selected)
 
     }
 
+    @IBAction func actionCheckBoxTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1, delay: 0.1, options: .curveLinear, animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            
+        }) { (success) in
+            UIView.animate(withDuration: 0.1, delay: 0.1, options: .curveLinear, animations: {
+                sender.isSelected = !sender.isSelected
+                sender.transform = .identity
+            }, completion: nil)
+        }
+    }
 }
