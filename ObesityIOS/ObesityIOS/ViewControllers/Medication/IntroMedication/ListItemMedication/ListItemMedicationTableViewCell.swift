7e7bd1b4544cdca706b87b6c9857227a1e359bfd
Jorge Amores <jamoresortiz@gmail.com>
2018-06-05 15:24:13 +0200

    Se maquetan pantallas de medicamentos

---
+++ ObesityIOS/ObesityIOS/ViewControllers/Medication/IntroMedication/ListItemMedication/ListItemMedicationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListItemMedicationTableViewCell.swift
+//  ObesityIOS
+//
+//  Created by Jorge Amores Ortiz on 5/6/18.
+//  Copyright © 2018 Preobar. All rights reserved.
+//
+
+import UIKit
+
+class ListItemMedicationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
